9dfc646129f867e4985a2a91b3b840ab86be428d
Dmitry Moskowski <me@corpix.ru>
2014-08-12 23:37:26 +0400

    Prepared package to be uploaded to pypi

--- setup.py
+++ setup.py
@@ -2,12 +2,17 @@
 
 from distutils.core import setup
 
+__version__ = '0.1.0'
+
 setup(
     name='shortid',
-    version='0.1.0',
+    version=__version__,
     description='Short id generator',
     author='Dmitry Moskowski',
     author_email='me@corpix.ru',
-    url='http://github.com/corpix/shortid',
-    packages=['shortid']
+    url='https://github.com/corpix/shortid',
+    download_url='https://github.com/corpix/shortid/archive/{0}.tar.gz'.format(__version__),
+    keywords = ['short', 'id', 'uuid', 'shortid', 'tinyid'],
+    packages=['shortid'],
+    classifiers=[],
 )
